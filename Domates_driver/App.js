4d9dd6e954d2b25bf0675f67915fe081cb03a266
burninggun <willchengsters@gmail.com>
2019-04-21 09:24:33 -0700

    attempting google gmaps

--- Domates_driver/App.js
+++ Domates_driver/App.js
@@ -1,13 +1,16 @@
 import Home from './screens/home'
 import AvailablePackages from './screens/available_packages'
+import Drive from './screens/drive';
 import { createStackNavigator, createAppContainer } from 'react-navigation'
 
 
 
 
+
 const MainNavigator = createStackNavigator({
-  Home: {screen: Home},
-  AvailablePackages: {screen: AvailablePackages}
+  // Home: {screen: Home},
+  AvailablePackages: {screen: AvailablePackages},
+  Drive: {screen: Drive},
 }, {
 	// headerMode: 'none'
 })
